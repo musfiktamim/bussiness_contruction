3e349abfa70c28a9b10b181daf9bc416e1d2eb9e
musfiktamim <musfiktamim@gmail.com>
2025-05-24 02:14:21 +0600

    un used versa

--- src/app/(deshboard)/deshboard/blog/[id]/edit/page.tsx
+++ src/app/(deshboard)/deshboard/blog/[id]/edit/page.tsx
@@ -3,13 +3,16 @@ import prisma from '@/lib/PrismClient'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-async function ServiceEditpage({params}:{params:{id:string}}) {
+async function ServiceEditpage({params}:{params:{id:string}}): Promise<React.JSX.Element> {
+  
   const blogs = await prisma.blogs.findUnique({
     where:{
       id:params.id
     }
   })
+  
   if(!blogs) notFound();
+
   return (
     <div>
       <BlogEditBox item={blogs} />
