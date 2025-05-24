3d72c3917f2dc1518451dbc5e5dc9d834e8cea96
musfiktamim <musfiktamim@gmail.com>
2025-05-24 22:47:38 +0600

    asdaaasasdasd

--- src/lib/ConstSlider.js
+++ src/lib/ConstSlider.js
@@ -2,22 +2,6 @@ import image1 from "../../public/images/home-slide-1.jpg"
 import image2 from "../../public/images/home-slide-2.jpg"
 import image3 from "../../public/images/home-slide-3.jpg"
 
-import Serviceimage1 from "../../public/images/service-1.png"
-import Serviceimage2 from "../../public/images/service-2.png"
-import Serviceimage3 from "../../public/images/service-3.png"
-import Serviceimage4 from "../../public/images/service-4.png"
-import Serviceimage5 from "../../public/images/service-5.png"
-import Serviceimage6 from "../../public/images/service-6.png"
-import Serviceimage7 from "../../public/images/service-7.png"
-import Serviceimage8 from "../../public/images/service-8.png"
-
-import ReviewImage1 from "../../public/images/pic-1.png"
-import ReviewImage2 from "../../public/images/pic-2.png"
-import ReviewImage3 from "../../public/images/pic-3.png"
-import ReviewImage4 from "../../public/images/pic-4.png"
-import ReviewImage5 from "../../public/images/pic-5.png"
-import ReviewImage6 from "../../public/images/pic-6.png"
-
 
 
 export const sliderData = [
@@ -58,50 +42,6 @@ export const Detailes = [
 ]
 
 
-export const OurServices = [
-    {
-        image: Serviceimage1,
-        title: "building construction",
-        desc: "Building construction is a multifaceted process that transforms an idea into a tangible structure."
-    },
-    {
-        image: Serviceimage2,
-        title: "house renovation",
-        desc: "House renovation involves updating, improving, or restoring residential structures to enhance their appearance, functionality, and value."
-    },
-    {
-        image: Serviceimage3,
-        title: "architechture design",
-        desc: "Architecture design involves the process of planning, designing, and constructing buildings and other physical structures."
-    },
-    {
-        image: Serviceimage4,
-        title: "material supply",
-        desc: "Material supply in the context of construction involves providing various types of materials needed for building projects."
-    },
-    {
-        image: Serviceimage5,
-        title: "construction consultant",
-        desc: "Construction consultants play a crucial role in the building industry by offering expertise in project management, planning, design, and construction oversight."
-    },
-    {
-        image: Serviceimage6,
-        title: "interior design",
-        desc: "Interior designers use various elements such as color, furniture, lighting, and decor to create functional and beautiful spaces."
-    },
-    {
-        image: Serviceimage7,
-        title: "building maintenance",
-        desc: "Building maintenance involves the upkeep, repair, and general care of physical structures, ensuring they remain safe, functional, and aesthetically pleasing."
-    },
-    {
-        image: Serviceimage8,
-        title: "building renovation",
-        desc: "Building renovation involves the process of restoring, improving, or updating existing structures to enhance their functionality, aesthetics, and overall value."
-    },
-]
-
-
 export const links = [
     {
         name: "Home",
@@ -129,45 +69,6 @@ export const links = [
     },
 ]
 
-
-export const testimonials = [
-  {
-    name: 'Vardaan Sao',
-    image: ReviewImage1,
-    rating: 5,
-    text: `Building our new corporate headquarters was a major undertaking...`,
-  },
-  {
-    name: 'Ram Kurre',
-    image: ReviewImage2,
-    rating: 5,
-    text: `Choosing Construction Website for our dream home project was the best decision...`,
-  },
-  {
-    name: 'Priya Roy',
-    image: ReviewImage3,
-    rating: 5,
-    text: `As a property developer, I have worked with many construction companies...`,
-  },
-  {
-    name: 'Priya Roy',
-    image: ReviewImage4,
-    rating: 4,
-    text: `As a property developer, I have worked with many construction companies...`,
-  },
-  {
-    name: 'Priya Roy',
-    image: ReviewImage5,
-    rating: 3,
-    text: `As a property developer, I have worked with many construction companies...`,
-  },
-  {
-    name: 'Priya Roy',
-    image: ReviewImage6,
-    rating: 1,
-    text: `As a property developer, I have worked with many construction companies...`,
-  },
-];
 
 
 export const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
